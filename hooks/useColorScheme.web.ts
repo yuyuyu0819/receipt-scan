ba507d59fdd35accfd9c4b00e21c7c9664780529
yuyuyu0819 <94299431+yuyuyu0819@users.noreply.github.com>
2025-12-24 13:05:50 +0900

    Use metro.config.js for Windows compatibility

---
+++ hooks/useColorScheme.web.ts
@@ -0,0 +1,15 @@
+import { useSyncExternalStore } from 'react';
+import { Appearance } from 'react-native';
+
+const subscribe = (callback: () => void) => {
+  const subscription = Appearance.addChangeListener(callback);
+  return () => subscription.remove();
+};
+
+export function useColorScheme() {
+  return useSyncExternalStore(
+    subscribe,
+    Appearance.getColorScheme,
+    Appearance.getColorScheme
+  );
+}
